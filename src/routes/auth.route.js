da90db59d09c8876caaa0e3d84ca3f6caf22d0c4
tuanhhdev <tuananhteves120@gmail.com>
2025-04-29 13:43:07 +0700

    feature: tạo middlware validation cho api register

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { authController } from "../controllers/auth.controller";
+import { validate } from "../common/middlewares/validate.middleware";
+import { registerSchema } from "../common/validations/auth.validation";
 
 const authRouter = express.Router();
 
-authRouter.post("/register", authController.register);
+authRouter.post("/register", validate(registerSchema), authController.register);
 
+authRouter.post("/login", authController.login);
 export default authRouter;
